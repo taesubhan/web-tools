8dba09ee29ad41741bd5c2ff3d64e1c7d5d8527e
taesubhan <tshan499@gmail.com>
2024-05-05 01:09:42 -0700

    Adjust the dropdown script to accept node element

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-function addDropdownEffect() {
-    // const allDropdownBtn = document.querySelectorAll('.dropdown-button');
-    // const allDropdownMenu = document.querySelectorAll('.dropdown-menu');
-    const allDropdown = document.querySelectorAll('.dropdown');
+function addDropdownEffect(dropdown) {
+    const btn = dropdown.querySelector('.dropdown-button');
+    const menu = dropdown.querySelector('.dropdown-menu')
 
-    allDropdown.forEach((dropdown) => {
-        const btn = dropdown.querySelector('.dropdown-button');
-        const menu = dropdown.querySelector('.dropdown-menu')
-
-        if (Boolean(btn) && Boolean(menu)) {
-            btn.addEventListener('click', () => {
-                menu.classList.toggle('hidden');
-            })
-        }
-
-        window.addEventListener('click', (e) => {
-            if (e.target.parentNode !== dropdown) {
-                const menu = dropdown.querySelector('.dropdown-menu');
-                if (!menu.classList.contains('hidden')) {
-                    menu.classList.add('hidden');
-                }
-            }
+    if (Boolean(btn) && Boolean(menu)) {
+        btn.addEventListener('click', () => {
+            menu.classList.toggle('hidden');
         })
-    })
+    }
 
-    
+    window.addEventListener('click', (e) => {
+        if (e.target.parentNode !== dropdown) {
+            const menu = dropdown.querySelector('.dropdown-menu');
+            if (!menu.classList.contains('hidden')) {
+                menu.classList.add('hidden');
+            }
+        }
+    })
 }
 
-addDropdownEffect();
+module.exports = addDropdownEffect;
